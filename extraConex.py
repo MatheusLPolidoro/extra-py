55b988dce66341a86f81142bd22aeec0ebbac80f
MatheusLPolidoro <mattpolidoro4@gmail.com>
2022-03-20 22:17:06 -0300

    Time

--- extraConex.py
+++ extraConex.py
@@ -1,5 +1,10 @@
 from win32com import client
 
+settletime = 1000
+
 system = client.GetObject('Reflection Workspace')
 screen = system.GetObject("Frame").view(1).Control.Screen
 screen.putText2('X', 5, 14)
+screen.Wait(settletime)
+screen.putText2('1', 5, 14)
+
